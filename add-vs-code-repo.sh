9d8d6abb7bbf1bfd295347dcb19880b8fd281ab4
Valentine Tanasovich <valentine.tanasovich@gmail.com>
2025-09-05 17:45:45 +0200

    TYPO: sudo is not needed

--- add-vs-code-repo.sh
+++ add-vs-code-repo.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 # Import GPG key for VS Code repo
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
 
 # Add repo
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
