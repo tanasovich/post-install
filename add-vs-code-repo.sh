cb1a1df60a2630c2ef2cfd137d5e9b5a1b58d81e
Valentine Tanasovich <valentine.tanasovich@gmail.com>
2025-09-05 17:47:32 +0200

    TYPO: sudo is not needed

--- add-vs-code-repo.sh
+++ add-vs-code-repo.sh
@@ -4,4 +4,4 @@
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 
 # Add repo
-echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | tee /etc/yum.repos.d/vscode.repo > /dev/null
