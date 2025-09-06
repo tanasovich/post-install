fb356bf752ea49d05b4fb66ccca65986a8a20266
Valentine Tanasovich <valentine.tanasovich@gmail.com>
2025-09-06 14:39:03 +0200

    RPM would install packages without script

--- install-packages.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-if [ ! -f assets/package-list.txt ]; then
-    echo "List of installation packages is not found!"
-    exit -1
-fi
-
-dnf install -y $(cat assets/package-list.txt)
